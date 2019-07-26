60c6db87aa085e90ef5fb4ea38e3365db9811499
Shahwar Spall <shevy.spall@gmail.com>
2019-07-26 04:40:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,7 @@ def adding_matz
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C":yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]]
+          :languages => ["C"]
         }
      }
 
