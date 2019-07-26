9d1905c97a94e4ee57b12bc3951ea777a2269a3d
Shahwar Spall <shevy.spall@gmail.com>
2019-07-26 05:19:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     programmer_hash
     end
 
 def adding_to_dennis
@@ -127,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
 
 end
